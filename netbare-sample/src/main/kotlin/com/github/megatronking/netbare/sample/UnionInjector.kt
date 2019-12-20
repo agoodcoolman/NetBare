b2130db15a17fbcd9e618786375b6549398a0163
jinmingkai <agoodcoolman@gmail.com>
2019-12-20 09:14:13 +0800

    可以拦截并且自己拼图了。

--- netbare-sample/src/main/kotlin/com/github/megatronking/netbare/sample/UnionInjector.kt
+++ netbare-sample/src/main/kotlin/com/github/megatronking/netbare/sample/UnionInjector.kt
@@ -2,10 +2,7 @@ package com.github.megatronking.netbare.sample
 
 import android.util.Log
 import com.github.megatronking.netbare.NetBareUtils
-import com.github.megatronking.netbare.http.HttpBody
-import com.github.megatronking.netbare.http.HttpRequest
-import com.github.megatronking.netbare.http.HttpRequestHeaderPart
-import com.github.megatronking.netbare.http.HttpResponseHeaderPart
+import com.github.megatronking.netbare.http.*
 import com.github.megatronking.netbare.injector.InjectorCallback
 import com.github.megatronking.netbare.injector.SimpleHttpInjector
 import com.github.megatronking.netbare.io.HttpBodyInputStream
@@ -24,6 +21,7 @@ class UnionInjector:SimpleHttpInjector( ){
     companion object {
         const val TAG = "UnionInjector"
     }
+    private var mHoldResponseHeader: HttpResponseHeaderPart? = null
 
     override fun sniffRequest(request: HttpRequest): Boolean {
         // 进去到签到页面的
@@ -48,16 +46,21 @@ class UnionInjector:SimpleHttpInjector( ){
         //        // 获取手机加密内容
         val params = String(body.toBuffer().array())
 //      encryptMobile=7f1efd01973e0200752ea77a3e0715a8&invitationCode=4xa7Fd84t
-
+        // 替换然后返回
         val replace = params.replace(params.split("=")[2], "5lGgy9Z6R")
+        // 变成二进制
         val wrap = ByteBuffer.wrap(replace.toByteArray())
-
-        super.onRequestInject(request, body, callback)
+//        callback.onFinished(ByteStream(replace.toByteArray()))
+        // 用父类的调用下回调
+        super.onRequestInject(request, HttpRawBody(wrap), callback)
     }
 
     override fun onRequestInject(header: HttpRequestHeaderPart, callback: InjectorCallback) {
         val query = header.uri().query
-
+        val injectHeader = header!!
+                .newBuilder()
+                .replaceHeader("Content-Length", "71")
+                .build()
 //        // 获取手机加密内容
 //        val encryptMobileParam = header.uri().getQueryParameter("encryptMobile")
 //        // 获取邀请码，然后用自己的反射注入进去
@@ -66,7 +69,7 @@ class UnionInjector:SimpleHttpInjector( ){
 //        ShareUtils.setEncryptMobile(encryptMobileParam)
 //        header.uri().buildUpon()
 //        header.uri().path.replace(code, "5lGgy9Z6R")
-        callback.onFinished(header)
+        callback.onFinished(injectHeader)
     }
 
 }
